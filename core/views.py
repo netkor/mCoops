940bb254017bfb2c6aca3cf7940261b794a20f12
Netra Koirala <43111478+netkor@users.noreply.github.com>
2024-10-31 13:47:24 +0545

    working

--- core/views.py
+++ core/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Setting , Category,SocialLink
+
 
 # Create your views here.
+def site_settings(request):
+    # Fetch the first Setting object (or adjust as needed)
+    setting = Setting.objects.first()
+    categories=Category.objects.all()
+    social_links = SocialLink.objects.all()  # Fetch all social links
+    return {
+        'site_setting': setting,
+        'categories': categories,
+        'social_links': social_links,
+    }
 
 def custom_upload_function(instance, filename):
     # Your logic for handling the upload
